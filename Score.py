602d3eac1dae92d0a7e8dc6c33b7c091a8c9150a
Kroll <113384296+KpoJleBapKa@users.noreply.github.com>
2025-10-10 08:20:05 +0300

    v0.8 __init__() func implementation

--- Score.py
+++ Score.py
@@ -1,2 +1,7 @@
 class Score:
-    """s"""
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.score = 0  # Початковий рахунок спійманих яєць
+        self.lost = 0  # Початкова кількість пропущених яєць
+        self.text = ""  # Перемінна для тексту
+        self.show_text()  # Метод для відображення рахунку на полотні
