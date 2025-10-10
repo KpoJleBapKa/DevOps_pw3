411f0b4828246e0264f7adef360cf5e0435f4aea
Kroll <113384296+KpoJleBapKa@users.noreply.github.com>
2025-10-10 08:22:36 +0300

    v0.11 catched_egg() func implementation

--- Score.py
+++ Score.py
@@ -5,10 +5,13 @@ class Score:
         self.lost = 0  # Початкова кількість пропущених яєць
         self.text = ""  # Перемінна для тексту
         self.show_text()  # Метод для відображення рахунку на полотні
-        
+
     def show_text(self):
         # Метод створює текст на полотні або оновлює його
         if (self.text == ""):
             self.text = self.canvas.create_text(350, 10, text=f"Спіймав: 0 Пропустив: 0", font=('Helvetica', 16))
         else:
             self.canvas.itemconfig(self.text, text=f"Спіймав: {self.score} Пропустив: {self.lost}")
+    def catched_egg(self):
+        self.score += 1  # Збільшує рахунок спійманих
+        self.show_text()  # Оновлює текст на полотні з новим рахунком
