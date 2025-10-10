64b36154912ca082ee24f0b68b1af0eec05ff455
Kroll <113384296+KpoJleBapKa@users.noreply.github.com>
2025-10-10 08:28:54 +0300

    v0.18 updating main.py
    Implementation of main cycle of the game which will be creating new eggs randomly

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from tkinter import *
 from Catcher import Catcher
 from Score import Score
+from Egg import Egg
 
 tk = Tk()
 tk.title("Гра: Ловець!")
@@ -15,5 +16,13 @@ canvas.pack()
 
 score = Score(canvas)
 catcher = Catcher(canvas, 'blue', score)
+eggs = []
+while 1:  # Початок безкінечного циклу для ігрового процесу
+    if random.randint(1, 100) == 1:  # Випадкова перевірка (1% шанс)
+        eggs.append(Egg(canvas, 'red', score))  # Створення нового яйця та додавання до списку
+    tk.update_idletasks()  # Оновлення ігрового інтерфейсу без блокування інших завдань
+    tk.update()  # Повне оновлення всього ігрового вікна
+    time.sleep(0.01)  # Коротка пауза
+
 tk.update()
 time.sleep(1)
