e61c0fbd676182ff22141e3f0796a211a1f94065
Kroll <113384296+KpoJleBapKa@users.noreply.github.com>
2025-10-10 08:20:44 +0300

    v0.9 show_text() func implementation

--- Score.py
+++ Score.py
@@ -5,3 +5,10 @@ class Score:
         self.lost = 0  # Початкова кількість пропущених яєць
         self.text = ""  # Перемінна для тексту
         self.show_text()  # Метод для відображення рахунку на полотні
+        
+    def show_text(self):
+        # Метод створює текст на полотні або оновлює його
+        if (self.text == ""):
+            self.text = self.canvas.create_text(350, 10, text=f"Спіймав: 0 Пропустив: 0", font=('Helvetica', 16))
+        else:
+            self.canvas.itemconfig(self.text, text=f"Спіймав: {self.score} Пропустив: {self.lost}")
